c810920e5e55bbfba45b158c0d8798ef56fa3997
AANYA <aanya20419@iiitd.ac.in>
2022-02-08 11:59:34 +0530

    Time: 1 ms (98.89%), Space: 41.8 MB (5.27%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=0;
+       while(num>0){
+           sum+=num%10;
+           num=num/10;
+           if(sum>9){
+               sum=sum%10+sum/10;
+           }
+       } 
+        return sum;
+    }
+}
