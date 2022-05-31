f7899e57d205c4b4206de7ed38ebb62d7071ca84
AANYA <aanya20419@iiitd.ac.in>
2022-05-31 15:32:15 +0530

    Time: 2 ms (57.57%), Space: 72.5 MB (71.87%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -24,4 +24,41 @@ class Solution {
             return maxsum;
         }
     }
+    
+     public int maxSubArray2(int[] nums) {
+        int max_sum = 0;
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = i; j <= n - 1; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++)
+                    sum = sum + nums[k];
+                if (sum > max_sum) {
+                    max_sum = sum;
+                }
+            }
+        }
+        return max_sum;
+     }
+    
+    public int maxSubArray3(int[] nums) {
+        int max_sum = Integer.MIN_VALUE;;
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        for (int i = 0; i <= n - 1; i++) {
+            int sum = 0;
+            for (int j = i; j <= n - 1; j++) {
+                    sum = sum + nums[j];
+            if (sum > max_sum) {
+                    max_sum = sum;
+            }
+        }
+     }
+        return max_sum;
+}
 }
