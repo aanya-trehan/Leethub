7e242b0d6ee4d336245459428aa530d5fa75cb7f
AANYA <aanya20419@iiitd.ac.in>
2022-06-02 12:22:42 +0530

    Time: 3 ms (75.17%), Space: 49.8 MB (42.05%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
+    public List<Integer> majorityElementBetter(int[] nums) {
         HashMap <Integer,Integer> map=new HashMap<>();
         ArrayList<Integer> ans=new ArrayList<>();
         for(int num:nums){
@@ -11,5 +11,53 @@ class Solution {
             }
         }
         return ans;
+    }
+    
+    public List<Integer> majorityElement(int[] nums){
+        int num1=-1;
+        int num2=-1;
+        int count1=0;
+        int count2=0;
+        
+        List<Integer> ans= new ArrayList<>();
+            
+        for(int num:nums){
+            if(num==num1){
+                count1++;
+            }
+            else if (num==num2){
+                count2++;
+            }
+            else if(count1==0){
+                num1=num;
+                count1++;
+            }
+            else if(count2==0){
+                num2=num;
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
+        for(int num:nums){
+            if(num==num1){
+                count1++;
+            }
+            else if (num==num2){
+                count2++;
+            }
+        }
+        
+        if(count1>(nums.length/3)){
+            ans.add(num1);
+        }
+        if(count2>(nums.length/3)){
+            ans.add(num2);
+        }
+    return ans;
     }
 }
