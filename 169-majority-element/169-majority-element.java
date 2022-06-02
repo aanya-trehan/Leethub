6cb371ce89700a9b185fd944f649fc5bfe24df04
AANYA <aanya20419@iiitd.ac.in>
2022-06-02 11:45:44 +0530

    Time: 2 ms (82.45%), Space: 56.7 MB (19.03%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0;
+        int ele=nums[0];
+        for(int num:nums){
+            if(count==0){
+                ele=num;
+            }
+            if(ele==num){
+                count++;
+            }
+            else{
+                count--;
+            }
+                
+            
+        }
+        return ele;
+    }
+}
