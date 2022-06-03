8b8d8efa315d6c8c1b27cb3c2078454e12ab5302
AANYA <aanya20419@iiitd.ac.in>
2022-06-03 14:37:37 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (90.91%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix1(int[][] matrix, int target) {
         int r=matrix.length;
         int c=matrix[0].length;
         int i=0;
@@ -18,4 +18,33 @@ class Solution {
         return false;
         
     }
+    //TC- O(n+m)
+    
+    
+    //Using binary search log(N+M)
+     public boolean searchMatrix(int[][] matrix, int target) {
+         if(matrix.length==0){
+             return false;
+         }
+         int r=matrix.length;
+         int c=matrix[0].length;
+         int l=0;
+         int h=(r*c)-1;
+         while(l<=h){
+             int m=l+(h-l)/2;
+             int cmpr=matrix[m/c][m%c];
+             if(target==cmpr){
+                 return true;
+             }
+             else if(target<cmpr){
+                 h=m-1;
+             }
+             else if(target>cmpr){
+                 l=m+1;
+             }
+         }
+         return false;
+     }
+    
+   
 }
