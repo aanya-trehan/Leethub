f0b8c26a255f0db559a7b7356341aff9a668d63d
AANYA <aanya20419@iiitd.ac.in>
2022-06-03 17:27:35 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (85.15%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -26,9 +26,12 @@ class Solution {
         int N=m+n-2;
         int r=Math.min(m,n)-1;
         double res = 1;
-        for(int i=1;i<=r;i++){
-            //ans=(ans*(N-i))/(i+1);
-            res = res * (N - r + i) / i;
+        // for(int i=1;i<=r;i++){
+        //     //ans=(ans*(N-i))/(i+1);
+        //     res = res * (N - r + i) / i;
+        // }
+        for(int i=0;i<r;i++){
+            res=res*(N-i)/(i+1);
         }
         return (int)res;
     }
