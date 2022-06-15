202a4c5bee839ee2e7cfe3aa772efe85bc2aba52
AANYA <aanya20419@iiitd.ac.in>
2022-06-15 19:51:36 +0530

    Time: 1 ms (99.58%), Space: 55.7 MB (27.76%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -15,8 +15,20 @@ public class Solution {
         ListNode d2 = head2;
 
         while(d1 != d2) {
-            d1 = d1 == null? head2:d1.next;
-            d2 = d2 == null? head1:d2.next;
+            if(d1==null){
+                d1=head2;
+            }
+            else{
+                d1=d1.next;
+            }
+            if(d2==null){
+                d2=head1;
+            }
+            else{
+                d2=d2.next;
+            }
+            // d1 = d1 == null? head2:d1.next;
+            // d2 = d2 == null? head1:d2.next;
         }
 
         return d1;
