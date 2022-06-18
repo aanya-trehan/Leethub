fd6b9ffc7e45404276d2da76ef54f516bec42632
AANYA <aanya20419@iiitd.ac.in>
2022-06-19 01:32:46 +0530

    Time: 3 ms (55.37%), Space: 42.1 MB (48.93%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isValid(String s) {
+    public boolean isValid1(String s) {
         
         Stack<Character> stack=new Stack<Character>();
         
@@ -31,6 +31,21 @@ class Solution {
             }
         }
         return stack.isEmpty();
+    }
+    
+     public boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+            for (char it : s.toCharArray()) {
+                if (it == '(' || it == '[' || it == '{')
+                    st.push(it);
+                else {
+                    if(st.isEmpty()) return false;
+                    char ch = st.pop(); 
+                    if((it == ')' && ch == '(') ||  (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;
+                    else return false;
+                }
+            }
+            return st.isEmpty();
     }
 }
 
