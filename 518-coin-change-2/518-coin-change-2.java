34cd8585dcefbb97b04f73e7f05dd2569ed662e6
AANYA <aanya20419@iiitd.ac.in>
2022-06-21 01:34:33 +0530

    Time: 17 ms (29.50%), Space: 54.3 MB (6.28%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,27 +1,25 @@
 class Solution {
-    public int change(int T, int[] coins) {
-        long dp[][]=new long[coins.length][T+1];
-        for(long row[]: dp)
-        Arrays.fill(row,-1);
-        return countWaysToMakeChangeUtil(coins,coins.length-1,T,dp);
+    public int change(int T, int[] arr) {
+        int n=arr.length;
+        long dp[][]=new long[n][T+1];
+        for(int i=0;i<=T;i++){
+            if(i%arr[0]==0)
+                dp[0][i]=1;
+        }
+
+        for(int ind=1; ind<n;ind++){
+            for(int target=0;target<=T;target++){
+                long notTaken = dp[ind-1][target];
+
+                long taken = 0;
+                if(arr[ind]<=target)
+                    taken = dp[ind][target-arr[ind]];
+
+                dp[ind][target] = notTaken + taken;
+            }
+        }
+
+        return (int)dp[n-1][T];
     }
-     public int countWaysToMakeChangeUtil(int[] arr, int ind, int T,long[][] dp){
-        if(ind == 0){
-        if(T%arr[0]==0)
-        return 1;
-        else
-        return 0;
-    }
-    
-    if(dp[ind][T]!=-1)
-        return (int)dp[ind][T];
-        
-    long notTaken = countWaysToMakeChangeUtil(arr,ind-1,T,dp);
-    
-    long taken = 0;
-    if(arr[ind] <= T)
-        taken = countWaysToMakeChangeUtil(arr,ind,T-arr[ind],dp);
-        dp[ind][T] = notTaken + taken;
-    return (int)dp[ind][T];
-     }
 }
+        
