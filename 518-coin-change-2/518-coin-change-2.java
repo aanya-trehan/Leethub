25c68eb2b33e17691330b3f39fc0fdd8df11421d
AANYA <aanya20419@iiitd.ac.in>
2022-06-21 01:36:33 +0530

    Time: 15 ms (36.09%), Space: 53.9 MB (6.30%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,25 +1,29 @@
 class Solution {
     public int change(int T, int[] arr) {
-        int n=arr.length;
-        long dp[][]=new long[n][T+1];
-        for(int i=0;i<=T;i++){
-            if(i%arr[0]==0)
-                dp[0][i]=1;
+        long[] prev=new long[T+1];
+    //Initializing base condition
+    for(int i=0;i<=T;i++){
+        if(i%arr[0]==0)
+            prev[i]=1;
+        // Else condition is automatically fulfilled,
+        // as prev array is initialized to zero
+    }
+    
+    for(int ind=1; ind<arr.length;ind++){
+        long cur[]=new long[T+1];
+        for(int target=0;target<=T;target++){
+            long notTaken = prev[target];
+            
+            long taken = 0;
+            if(arr[ind]<=target)
+                taken = cur[target-arr[ind]];
+                
+            cur[target] = notTaken + taken;
         }
-
-        for(int ind=1; ind<n;ind++){
-            for(int target=0;target<=T;target++){
-                long notTaken = dp[ind-1][target];
-
-                long taken = 0;
-                if(arr[ind]<=target)
-                    taken = dp[ind][target-arr[ind]];
-
-                dp[ind][target] = notTaken + taken;
-            }
-        }
-
-        return (int)dp[n-1][T];
+        prev = cur;
+    }
+    
+    return (int)prev[T];
     }
 }
         
