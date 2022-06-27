ca1566ea60d925cd121b5b4b1228fb93baf60c21
AANYA <aanya20419@iiitd.ac.in>
2022-06-27 23:50:17 +0530

    Time: 1 ms (87.85%), Space: 43.4 MB (50.20%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<List<Integer>>();
+        helper(nums,0,ans, new ArrayList<Integer>());
+        return ans;
+    }
+    
+    public void helper(int[] nums, int index , List<List<Integer>> ans, List<Integer> temp ){
+        if(index==nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        temp.add(nums[index]);
+        helper(nums,index+1,ans,temp);
+        temp.remove(temp.size()-1);
+        helper(nums,index+1,ans,temp);
+        
+    }
+}
