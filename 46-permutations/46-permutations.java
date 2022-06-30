780e8488da3fb484e37c36427f17910e1f90aad8
AANYA <aanya20419@iiitd.ac.in>
2022-06-30 23:32:44 +0530

    Time: 3 ms (43.72%), Space: 44.9 MB (38.73%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,5 +1,39 @@
 class Solution {
+    //optimal sol
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        recurpermute(0,nums,ans);
+        return ans;
+    }
+    
+    private void recurpermute(int ind,int[] nums,List<List<Integer>> ans){
+        if(ind==nums.length){
+            List<Integer> ds=new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                ds.add(nums[i]);
+            }
+            ans.add(ds);
+            return;
+        }
+        for(int i=ind;i<nums.length;i++){
+            swap(ind,i,nums);
+            recurpermute(ind+1,nums,ans);
+             swap(ind,i,nums);
+        }
+    }
+    
+    private void swap(int ind1,int ind2,int[] nums){
+        int temp=nums[ind1];
+        nums[ind1]=nums[ind2];
+        nums[ind2]=temp;
+    }
+    
+    
+    
+    
+    
+    // alternate sol
+    public List<List<Integer>> permute2(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
         boolean[] freq=new boolean[nums.length];
         List<Integer> ds=new ArrayList<>();
