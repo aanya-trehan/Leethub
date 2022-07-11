d8992085c76398c19f1eeee9f23ce7c0b9628d49
AANYA <aanya20419@iiitd.ac.in>
2022-07-11 22:50:38 +0530

    Time: 2 ms (82.97%), Space: 44.4 MB (72.13%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -15,20 +15,20 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        int[] count=new int[1];
         List<List<Integer>> ans=new ArrayList<>();
-        findPaths(root,targetSum,new ArrayList<Integer>(),ans);
-        return ans;
-        
+        findPaths(root,targetSum,new ArrayList<Integer>(),ans,count);
+        return ans; 
     }
-    public void findPaths(TreeNode root, int sum, ArrayList<Integer> current,  List<List<Integer>> paths){
+    public void findPaths(TreeNode root, int sum, ArrayList<Integer> current, List<List<Integer>> paths,int[] count){
         if(root == null) return;
         current.add(root.val);
-        if(sum == root.val && root.left == null && root.right == null)
-                paths.add(new ArrayList<>(current));
-
-        findPaths(root.left, sum - root.val, current, paths);
-        findPaths(root.right, sum - root.val, current, paths);
-
+        if(sum == root.val && root.left == null && root.right == null){
+            paths.add(new ArrayList<>(current));
+            count[0]++;
+        }
+        findPaths(root.left, sum - root.val, current, paths,count);
+        findPaths(root.right, sum - root.val, current, paths,count);
         current.remove(current.size() - 1);
     }
 }
